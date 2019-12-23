ea2e3930db2089b5f2c4ce7d86e2b5c095dd9645
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2019-12-22 19:42:59 -0500

    weather page styling added

--- src/MarsWeather/LatestDayCard.js
+++ src/MarsWeather/LatestDayCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const LatestDayCard = (props) => {
 
   if (props.latestDayInfo) {
-    const { AT, First_UTC, Season} = props.latestDayInfo  
+    const { AT, First_UTC } = props.latestDayInfo  
     // HWS, PRE, WD 
     const date = new Date(First_UTC).toDateString()
 
@@ -13,7 +13,6 @@ const LatestDayCard = (props) => {
           <p className="ldSol">SOL {props.sol}</p>
           <p className="ldDate">{date.split(" ").slice(0,1)} {date.split(" ").slice(1,3).join(" ")} {date.split(" ").slice(-1)}</p>
         </div>
-        <p className="ldContent">{Season}</p>
         <p className="ldContent">{Math.round(AT["mx"])}°C</p>
         <i id="ldThermoIcon" className="thermometer icon"/>
         <p className="ldContent">{Math.round(AT["mn"])}°C</p>
